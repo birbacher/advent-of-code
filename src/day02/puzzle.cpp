678250f0f772d2479a351987472df6025c0a1440
Frank Birbacher <frank.birbacher@gmail.com>
2022-09-29 22:57:19 +0200

    Day 02 puzzle A

---
+++ src/day02/puzzle.cpp
@@ -0,0 +1,41 @@
+#include "day02/puzzle.hpp"
+
+#include "iosupport/iosupport.hpp"
+
+#include <charconv>
+#include <optional>
+#include <deque>
+#include <istream>
+#include <string>
+#include <stdexcept>
+
+namespace advent::day02 {
+
+void puzzleA(std::istream& input, std::ostream& output)
+{
+    std::string command;
+    int amount;
+
+    int horizontal = 0, depth = 0;
+    while (input >> command >> amount) {
+        if (command == "forward") {
+            horizontal += amount;
+        }
+        else if (command == "down") {
+            depth += amount;
+        }
+        else if (command == "up") {
+            depth -= amount;
+        }
+        else {
+            throw std::runtime_error("Unknown command: " + command);
+        }
+    }
+    output << horizontal * depth;
+}
+
+void puzzleB(std::istream& input, std::ostream& output)
+{
+}
+
+}
