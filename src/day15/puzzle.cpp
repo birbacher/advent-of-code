1e7f24a266203fb167fad223baaf1f84e0cc1ad5
Frank Birbacher <frank.birbacher@gmail.com>
2022-12-19 23:36:20 +0100

    Day 15 puzzle B

--- src/day15/puzzle.cpp
+++ src/day15/puzzle.cpp
@@ -91,20 +91,23 @@ std::pair<std::ptrdiff_t, std::ptrdiff_t> rangeInRow(RangedSensor rs,
     return {rs.position.col - deltaCol, rs.position.col + deltaCol + 1};
 }
 
-} // namespace
+std::vector<Line> loadSensors(std::istream &input) {
+    return std::vector(std::istream_iterator<Line>{input},
+                       std::istream_iterator<Line>{});
+}
 
-template <> void puzzleA<2022, 15>(std::istream &input, std::ostream &output) {
-    std::vector sensors(std::istream_iterator<Line>{input},
-                        std::istream_iterator<Line>{});
-
-    // static constexpr std::ptrdiff_t targetRow = 10;
-    static constexpr std::ptrdiff_t targetRow = 2'000'000;
-
+std::vector<std::pair<std::ptrdiff_t, std::ptrdiff_t>>
+loadRanges(std::vector<Line> const &sensors, std::ptrdiff_t targetRow) {
     std::vector<std::pair<std::ptrdiff_t, std::ptrdiff_t>> ranges;
-    std::transform(
-        sensors.begin(), sensors.end(), std::back_inserter(ranges),
-        [](Line line) { return rangeInRow(makeRanged(line), targetRow); });
+    std::transform(sensors.begin(), sensors.end(), std::back_inserter(ranges),
+                   [targetRow](Line line) {
+                       return rangeInRow(makeRanged(line), targetRow);
+                   });
+    return ranges;
+}
 
+std::pair<std::ptrdiff_t, std::ptrdiff_t> findLowHigh(
+    std::vector<std::pair<std::ptrdiff_t, std::ptrdiff_t>> const &ranges) {
     std::ptrdiff_t lowest{}, highest{};
     bool first = true;
     for (auto [low, high] : ranges) {
@@ -118,9 +121,14 @@ template <> void puzzleA<2022, 15>(std::istream &input, std::ostream &output) {
             lowest = std::min(lowest, low);
             highest = std::max(highest, high);
         }
-        //output << lowest << " - " << highest << '\n';
     }
+    return {lowest, highest};
+}
 
+std::vector<bool> loadPositions(
+    std::vector<Line> const &sensors,
+    std::vector<std::pair<std::ptrdiff_t, std::ptrdiff_t>> const &ranges,
+    std::ptrdiff_t lowest, std::ptrdiff_t highest, std::ptrdiff_t targetRow) {
     const std::ptrdiff_t offset = lowest;
     const std::ptrdiff_t length = highest - lowest;
 
@@ -135,10 +143,55 @@ template <> void puzzleA<2022, 15>(std::istream &input, std::ostream &output) {
             set[line.beacon.col - offset] = false;
         }
     }
+    return set;
+}
+
+} // namespace
+
+template <> void puzzleA<2022, 15>(std::istream &input, std::ostream &output) {
+    // static constexpr std::ptrdiff_t targetRow = 10;
+    static constexpr std::ptrdiff_t targetRow = 2'000'000;
+
+    const std::vector sensors = loadSensors(input);
+    const std::vector ranges = loadRanges(sensors, targetRow);
+
+    auto const [lowest, highest] = findLowHigh(ranges);
+
+    const std::vector<bool> set =
+        loadPositions(sensors, ranges, lowest, highest, targetRow);
 
     output << std::count(set.begin(), set.end(), true) << '\n';
 }
 
-template <> void puzzleB<2022, 15>(std::istream &input, std::ostream &output) {}
+template <> void puzzleB<2022, 15>(std::istream &input, std::ostream &output) {
+    // static constexpr std::ptrdiff_t targetRow = 10;
+    // static constexpr std::ptrdiff_t minCo = 0;
+    // static constexpr std::ptrdiff_t maxCo = 20;
+    static constexpr std::ptrdiff_t targetRow = 2'000'000;
+    static constexpr std::ptrdiff_t minCo = 0;
+    static constexpr std::ptrdiff_t maxCo = 4'000'000;
+    static constexpr std::ptrdiff_t resultFactor = 4'000'000;
+
+    const std::vector sensors = loadSensors(input);
+    for (std::ptrdiff_t y = minCo; y <= maxCo; ++y) {
+        std::vector ranges = loadRanges(sensors, y);
+        std::sort(ranges.begin(), ranges.end());
+        std::ptrdiff_t curX = minCo;
+        for (auto [l, h] : ranges) {
+            if (curX < l) {
+                output << (curX * resultFactor + y) << '\n';
+                return;
+            }
+            curX = std::max(curX, h);
+            if (curX > maxCo) {
+                break;
+            }
+        }
+    }
+    throw std::runtime_error("Exhausted search space");
+
+    // RelWithDebInfo
+    // runtime: real    0m5,843s
+}
 
 } // namespace advent::common
